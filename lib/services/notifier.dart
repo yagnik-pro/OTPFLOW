195706a99b3104e01ec0b38e42963d406596348e
yagnik-pro <mayaniyagnik84@gmail.com>
2026-09-11 09:24:35 +0530

    Update notifier.dart

--- lib/services/notifier.dart
+++ lib/services/notifier.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 /// Local notifications — fires when a new return OTP shows up.
